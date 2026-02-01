2d11dc45fc66451b6cb51fc957d90734393fe8bf
bivector <zvonkov.dalv@gmail.com>
2026-02-02 00:53:58 +0800

    full rewriting code 1

--- scripts/overrideCanvasSize.js
+++ scripts/overrideCanvasSize.js
@@ -1,11 +1,4 @@
 const canvas = document.getElementById("canvas");
-const canvas2 = document.getElementById("animation");
 
-function resize() {
-  canvas2.width = canvas2.clientWidth;
-  canvas.width = canvas.clientWidth;
-  canvas2.height = canvas2.clientWidth;
-  canvas.height = canvas.clientHeight;
-}
-
-resize();
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
